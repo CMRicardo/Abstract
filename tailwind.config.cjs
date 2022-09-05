2b6bfef3294811cb1e235e5a9b76bf4775e75faf
CMRicardo <ricardocorrales84@gmail.com>
2022-09-04 19:58:38 -0600

    Add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#4C5FD5",
+        headerBg: "#dadbf1",
+        background: "#fff",
+      },
+      fontFamily: {
+        sans: "America",
+        bold: "America-bold",
+      },
+    },
+  },
+  plugins: [],
+};
