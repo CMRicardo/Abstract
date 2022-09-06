9c4fd2bb92e740c0e8f45855c6bbd609435f6200
CMRicardo <ricardocorrales84@gmail.com>
2022-09-06 10:36:08 -0600

    Fix some warning and remove carets from package.json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,37 @@ import { Menu } from "../icons/Menu";
 import { Search } from "../icons/Search";
 import { MobileMenu } from "./MobileMenu";
 
-export const Navbar = () => {
+export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <nav class="bg-black text-white p-4 md:px-16 md:py-8 text-2xl flex justify-between items-center">
-        <h1 class="flex gap-2 m-0 p-0">
+      <nav className="bg-black text-white p-4 md:px-16 md:py-8 text-2xl flex justify-between items-center">
+        <h1 className="flex gap-2 m-0 p-0">
           <span>
             <Logo />
           </span>{" "}
           | Help Center
         </h1>
 
-        <div class="flex gap-4 md:hidden">
+        <div className="flex gap-4 md:hidden">
           <Search height={24} width={24} />
           <button onClick={() => setIsOpen(!isOpen)}>
             <Menu height={24} width={24} />
           </button>
         </div>
 
-        <ul class="hidden md:flex gap-4">
+        <ul className="hidden md:flex gap-4">
           <li>
             <a
-              class="bg-[#191a1b] px-4 py-2 rounded border-background border"
+              className="bg-[#191a1b] px-4 py-2 rounded border-background border"
               href="/"
             >
               Submit a request
             </a>
           </li>
           <li>
-            <a class="bg-accent px-6 py-2 rounded" href="/">
+            <a className="bg-accent px-6 py-2 rounded" href="/">
               Sign in
             </a>
           </li>
@@ -43,4 +43,4 @@ export const Navbar = () => {
       {isOpen && <MobileMenu />}
     </>
   );
-};
+}
